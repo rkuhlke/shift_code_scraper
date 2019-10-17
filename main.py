4d96d33424af4781de6ba999890d5a93b5e4d518
Robert <bkuhlke@yahoo.com>
2019-10-17 03:22:35 -0400

    adding logic to send two messages 1 sends the code for easy copy and paste into shift 2 sends a description of what the code gives and when it expires

--- main.py
+++ main.py
@@ -2,34 +2,74 @@
 Borderlands Web Scraper
 """
 import requests
-import urllib
 
 from bs4 import BeautifulSoup as bs
 
+BRODERLANDS_BOT = -296659970
+TEST = -380161856
 
-def code_parser():
-    get_page = requests.get('https://shift.orcicorn.com/shift-code/')
+
+# def date():
+#     x = datetime.today()
+#     return x.strftime("%a, %d %b %Y")
+
+
+def build_soup():
+    """
+    builds the html parser
+    :return: soup
+    """
+    get_page = requests.get('https://shift.orcicorn.com/index.xml')
     soup = bs(get_page.text, 'html.parser')
 
-    shift_list = []
-    for code in soup.find_all('article'):
-        code.get('href')
-        shift = code.text.split('\n')
-        shift_list.append({
-            'Code': shift[0],
-            'Date': shift[1]
-        })
-    return shift_list
+    return soup
 
 
-def send_to_telegram():
-    code = requests.get('https://api.telegram.org/bot924108836:AAHZykalHR8INIwplZERIwibUtDnUUdQN-8/'
-                        'sendMessage?chat_id=-296659970&text={}'.format(code_parser()))
+def shift_code():
+    """
+    grabs the shift code
+    :return: string
+    """
+    compare_list = []
+
+    code = build_soup().item.title.text
+
+    return code
+
+
+def description():
+    """
+    grabs the description of the shift code
+    :return: list
+    """
+
+    des_list = []
+    des = build_soup().item.description.text.split('<br>')
+    for item in des:
+        des_list.append(item)
+    note = des_list[-1].split(':')
+    reward = des_list[1].split(':')
+    game = des_list[2].split(':')
+
+    string = f'Game: {game[1]}\nReward: {reward[1]}\nNote: {note[1]}'
+
+    return string
+
+
+def send_to_telegram(group, text):
+    code = requests.get('https://api.telegram.org/bot924108836:'
+                        'AAHZykalHR8INIwplZERIwibUtDnUUdQN-8/'
+                        f'sendMessage?chat_id={group}'
+                        '=&text={}'.format(text))
     return code
 
 
 def main():
-    send_to_telegram()
+    text = description()
+    code = shift_code()
+
+    _code = send_to_telegram(TEST, code)
+    _description = send_to_telegram(TEST, text)
 
 
 if __name__ == '__main__':
